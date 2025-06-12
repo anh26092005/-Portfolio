88145345926b1523f7fcf337758dc28476002949
Anh <anhdlcwk@gmail.com>
2025-06-12 17:57:12 +0700

    a

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
 const menuIcon = document.getElementById("menu-icon");
 const navbar = document.querySelector(".navbar");
-
+const navLinks = document.querySelectorAll(".navbar a");
+const sections = document.querySelectorAll("section");
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
 
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".navbar a");
+const hideMenu = () => {
+  if (navbar.classList.contains("active")) {
+    navbar.classList.remove("active");
+  }
+};
 
-window.addEventListener("scroll", () => {
-  let current = "";
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    if (pageYOffset >= sectionTop - 60) {
-      current = section.getAttribute("id");
+navLinks.forEach((link) => {
+  link.addEventListener("click", hideMenu);
+});
+
+const observerOptions = {
+  root: null,
+  rootMargin: "-100px 0px -50% 0px",
+  threshold: 0,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const id = entry.target.getAttribute("id");
+
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+
+      const activeLink = document.querySelector(`.navbar a[href="#${id}"]`);
+      if (activeLink) {
+        activeLink.classList.add("active");
+      }
     }
   });
+}, observerOptions);
 
-  navLinks.forEach((link) => {
-    link.classList.remove("active");
-    if (link.getAttribute("href").slice(1) === current) {
-      link.classList.add("active");
-    }
-  });
+sections.forEach((section) => {
+  observer.observe(section);
 });
